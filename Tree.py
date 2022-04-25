d7b593c8d313316612ea0b69baa62b03b0ce130c
dan <danielmartins@usp.br>
2022-04-25 17:27:47 -0300

    main loop and tree module

--- Tree.py
+++ Tree.py
@@ -1,1 +1,167 @@
- 
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy as np
+
+
+def get_tree(position_x, position_y):
+    trunk = {
+        "vertex" : [(+0.0, +0.0),
+                    (+0.1, +0.0), 
+                    (+0.1, +0.5),
+                    (+0.0, +0.5)],
+        "translation": (position_x, position_y),
+        "color" : { "R":164.0/255,
+                    "G":116.0/255, 
+                    "B": 73.0/255},
+        "mode" : GL_POLYGON
+    } 
+
+    leaves = {
+        "vertex" : [(-0.2, 0.5),
+                    (0.05, 1.5  ), 
+                    (0.3, 0.5)],
+        "translation": (position_x, position_y),
+        "color" : { "R":97.0/255,
+                    "G":138.0/255, 
+                    "B":61.0/255},
+        "mode" : GL_TRIANGLES
+    } 
+
+    return [leaves, trunk]   
+
+
+# # INIT GLFW ###########################################################
+# glfw.init()
+# glfw.window_hint(glfw.VISIBLE, glfw.FALSE);
+# window = glfw.create_window(720, 600, "Main", None, None)
+# glfw.make_context_current(window)
+
+# # INIT PROGRAM ############################################################
+
+# vertex_code = """
+#         attribute vec2 position;
+#         uniform mat4 mat_transformation;
+#         void main(){
+#             gl_Position = mat_transformation*vec4(position,0.0,1.0);
+#         }
+#         """
+
+# fragment_code = """
+#         uniform vec4 color;
+#         void main(){
+#             gl_FragColor = color;
+#         }
+#         """
+
+# program  = glCreateProgram()
+# vertex   = glCreateShader(GL_VERTEX_SHADER)
+# fragment = glCreateShader(GL_FRAGMENT_SHADER)
+
+# glShaderSource(vertex, vertex_code)
+# glShaderSource(fragment, fragment_code)
+
+# glCompileShader(vertex)
+# if not glGetShaderiv(vertex, GL_COMPILE_STATUS):
+#     error = glGetShaderInfoLog(vertex).decode()
+#     print(error)
+#     raise RuntimeError("Erro de compilacao do Vertex Shader")
+
+# glCompileShader(fragment)
+# if not glGetShaderiv(fragment, GL_COMPILE_STATUS):
+#     error = glGetShaderInfoLog(fragment).decode()
+#     print(error)
+#     raise RuntimeError("Erro de compilacao do Fragment Shader") 
+
+# glAttachShader(program, vertex)
+# glAttachShader(program, fragment)
+
+# glLinkProgram(program)
+# if not glGetProgramiv(program, GL_LINK_STATUS):
+#     print(glGetProgramInfoLog(program))
+#     raise RuntimeError('Linking error')
+    
+# glUseProgram(program)
+
+# # create object ############################################################
+
+# # preparando espaço para 3 vértices usando 2 coordenadas (x,y)
+# position_x = -0.2
+# position_y = -0.5
+
+# trunk = {
+#     "vertex" : [(+0.0, +0.0),
+#                 (+0.1, +0.0), 
+#                 (+0.1, +0.5),
+#                 (+0.0, +0.5)],
+#     "translation": (position_x, position_y),
+#     "color" : { "R":164.0/255,
+#                 "G":116.0/255, 
+#                 "B": 73.0/255},
+#     "mode" : GL_POLYGON
+# } 
+
+# leaves = {
+#     "vertex" : [(-0.2, 0.5),
+#                 (0.05, 1.5  ), 
+#                 (0.3, 0.5)],
+#     "translation": (position_x, position_y),
+#     "color" : { "R":97.0/255,
+#                 "G":138.0/255, 
+#                 "B":61.0/255},
+#     "mode" : GL_TRIANGLES
+# }
+
+
+# total_vertices = leaves['vertex']+trunk['vertex']
+# vertices = np.zeros(len(total_vertices), [('position', np.float32, 2)])
+# vertices['position'] = total_vertices
+
+# objects = [leaves, trunk]
+
+# # upload data #########################################################################
+
+# buffer = glGenBuffers(1)
+# glBindBuffer(GL_ARRAY_BUFFER, buffer)
+# glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_DYNAMIC_DRAW)
+# glBindBuffer(GL_ARRAY_BUFFER, buffer)
+
+# stride = vertices.strides[0]
+# offset = ctypes.c_void_p(0)
+
+# loc = glGetAttribLocation(program, "position")
+# glEnableVertexAttribArray(loc)
+
+# glVertexAttribPointer(loc, 2, GL_FLOAT, False, stride, offset)
+
+# ######################################################################################
+
+
+# loc_color = glGetUniformLocation(program, "color")
+# glfw.show_window(window)
+
+# while not glfw.window_should_close(window):
+
+#     glfw.poll_events() 
+    
+#     glClear(GL_COLOR_BUFFER_BIT) 
+#     glClearColor(1.0, 1.0, 1.0, 1.0)
+
+#     vertex_acc = 0
+#     for obj in objects:
+
+#         mat_translation = np.array([1.0, 0.0, 0.0, obj["translation"][0], 
+#                                     0.0, 1.0, 0.0, obj["translation"][1], 
+#                                     0.0, 0.0, 1.0, 0.0, 
+#                                     0.0, 0.0, 0.0, 1.0], np.float32)
+
+#         loc = glGetUniformLocation(program, "mat_transformation")
+#         glUniformMatrix4fv(loc, 1, GL_TRUE, mat_translation)
+
+#         glUniform4f(loc_color, obj['color']['R'], obj['color']['G'], obj['color']['B'], 1.0)
+#         glDrawArrays(obj['mode'], vertex_acc, len(obj['vertex']))
+#         vertex_acc += len(obj['vertex'])
+
+#         glfw.swap_buffers(window)
+
+# glfw.terminate()
