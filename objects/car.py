42000d0168951e99423bd1f8566ca50b8ded7ba7
madukm <kawakamimadu@gmail.com>
2022-04-26 10:36:55 -0300

    Car reflected and on the surface

--- objects/car.py
+++ objects/car.py
@@ -1,11 +1,7 @@
 from .base import *
 
 def get_car(position_x, position_y):
-    shouldDrawTriangle = True
-    lastTriangle = 0
-    triangleMaxDeg = 28
-    degCounter = 0
-
+    
     def upd_func_body(trans, rot, keypress):
         if keypress == 263:
             trans = (trans[0] - 0.01, trans[1])
@@ -29,63 +25,57 @@ def get_car(position_x, position_y):
 
     body = {
         'vertex' : make_pair_vec([
-            (-0.25, -0.1),
-            (-0.25, -0.04),
-            (-0.2, 0),
-            (-0.15, 0),
-            (-0.1, 0.05),
-            (0.1, 0.05),
+            (0.25, -0.1),
+            (0.25, -0.04),
             (0.2, 0),
-            (0.25, 0),
-            (0.25, -0.1)
+            (0.15, 0),
+            (0.1, 0.05),
+            (-0.1, 0.05),
+            (-0.2, 0),
+            (-0.25, 0),
+            (-0.25, -0.1)
         ]),
         'translation' : (position_x, position_y),
         'rotation' : 0.0,
-        'color' : {"R":.78,
-                   "G":.39, 
-                   "B":.33},
+        'color' : {"R":1.,
+                   "G":.60, 
+                   "B":.0},
         'mode' : GL_POLYGON,
         'update' : upd_func_body
     }
 
-    cont = 0
-    wheel_in = np.zeros(6, POSITION_DT)
-    wheel = np.zeros(360, POSITION_DT)
-    for i in range(360):
-        fX = math.sin(i * DEG_IN_RAD) * 0.05
-        fY = math.cos(i * DEG_IN_RAD) * 0.05
-        wheel['position'][i] = (fX, fY)
+    def wheel(n_sides, radius):
+        cont = 0
+        wheel_in = np.zeros(n_sides, POSITION_DT)
+        wheel = np.zeros(360, POSITION_DT)
+        for i in range(360):
+            fX = math.sin(i * DEG_IN_RAD) * radius
+            fY = math.cos(i * DEG_IN_RAD) * radius
+            wheel['position'][i] = (fX, fY)
 
-        if i%60 == 0:
-            wheel_in['position'][cont] = (fX, fY)
-            cont = cont + 1
+            if i%(360/n_sides) == 0:
+                wheel_in['position'][cont] = (fX, fY)
+                cont = cont + 1
 
-    cont = 0
-    wheel_in2 = np.zeros(6, POSITION_DT)
-    wheel2 = np.zeros(360, POSITION_DT)
-    for i in range(360):
-        fX = math.sin(i * DEG_IN_RAD) * 0.05
-        fY = math.cos(i * DEG_IN_RAD) * 0.05
-        wheel2['position'][i] = (fX, fY)
+        return wheel, wheel_in
 
-        if i%60 == 0:
-            wheel_in2['position'][cont] = (fX, fY)
-            cont = cont + 1
-
+    wheel1, wheel_in1 = wheel(6, .05)
+    wheel2, wheel_in2 = wheel(6, .05)
+    
     wheel_in_color = {
-        "R":.0,
-        "G":.0, 
-        "B":.0,
+        "R":.5,
+        "G":.5, 
+        "B":.5,
     }
 
     wheel_out_color = {
         "R":.0,
         "G":.0, 
-        "B":1.
+        "B":.0
     }
 
     wheel_inside1 = {
-        'vertex' : wheel_in,
+        'vertex' : wheel_in1,
         'translation' : (position_x-0.15, position_y-0.10),
         'rotation' : 0.,
         'color' : wheel_in_color,
@@ -103,7 +93,7 @@ def get_car(position_x, position_y):
     }
 
     wheel_outside1 = {
-        'vertex' : wheel,
+        'vertex' : wheel1,
         'translation' : (position_x-0.15, position_y-0.10),
         'rotation' : 0.,
         'color' : wheel_out_color,
